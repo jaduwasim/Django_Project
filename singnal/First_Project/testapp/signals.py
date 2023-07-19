e2c8ffd819aedc4f870bebb5915017bf20fae327
jaduwasim <washimakram1631@gmail.com>
2023-07-19 11:56:58 +0530

    modified signals project

--- singnal/First_Project/testapp/signals.py
+++ singnal/First_Project/testapp/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import pre_init, pre_save, pre_delete, post_init, post_save, post_delete, pre_migrate, post_migrate
+from django.core.signals import request_started, request_finished, got_request_exception
 
 # This Will call When user login successfully.
 @receiver(user_logged_in, sender=User)
@@ -39,3 +41,110 @@ def login_failded(sender, credentials, request, **kwargs):
     print('credentials:',credentials)
     print(f'kwargs: {kwargs}')
 # user_login_failed.connect(login_failded) #We can use this instead of decorator @receiver(user_login_failed)
+
+@receiver(pre_save, sender=User)
+def at_ending_save(sender, instance, **kwargs):
+    print('========================================')
+    print('pre_save Singals')
+    print('sender:',sender)
+    print('instance:',instance)
+    print(f'kwargs: {kwargs}')
+
+@receiver(post_save, sender=User)
+def at_ending_save(sender, instance, created, **kwargs):
+    if created:
+        print('========================================')
+        print('Post Save signal')
+        print('New Record')
+        print('Sender:',sender)
+        print('Instance:', instance)
+        print(f'kwargs: {kwargs}')
+    else:
+        print('==============================')
+        print('post save signal')
+        print('User Updated')
+        print('sender:',sender)
+        print('instance:',instance)
+        print('created:',created)
+        print(f'kwargs: {kwargs}')
+
+@receiver(pre_delete, sender=User)
+def at_begging_delete(sender, instance, **kwargs):
+    print('============================')
+    print('At Beginninng (pre_delete)')
+    print('Sender:',sender)
+    print('instance:',instance)
+    print(f'kwargs: {kwargs}')
+
+@receiver(post_delete, sender=User)
+def at_end_deleting(sender, instance, **kwargs):
+    print('================================')
+    print('At Ending Delete (post deleting)')
+    print('Sender:',sender)
+    print('instance:',instance)
+    print(f'kwargs: {kwargs}')
+
+@receiver(pre_init, sender=User)
+def at_begeinning_init(sender, *args, **kwargs):
+    print('==================================')
+    print('Pre init signal')
+    print('sender:',sender)
+    print(f'args: {args}')
+    print(f'kwargs: {kwargs}')
+
+@receiver(post_init, sender=User)
+def at_ending_init(sender,*args,**kwargs):
+    print('===================================')
+    print('post init signal')
+    print('sender:',sender)
+    print(f'args: {args}')
+    print(f'kwargs: {kwargs}')
+
+@receiver(request_started)
+def at_beginning_request(sender, environ, **kwargs):
+    print('===================================')
+    print('At Beginning Request')
+    print('Sender:',sender)
+    print('environ:',environ)
+    print(f'kwargs: {kwargs}')
+
+@receiver(request_finished)
+def at_ending_request(sender, **kwargs):
+    print('===================================')
+    print('At End Request')
+    print('Sender:',sender)
+    print(f'kwargs: {kwargs}')
+
+@receiver(got_request_exception)
+def at_ending_request(sender, request, **kwargs):
+    print('===================================')
+    print('At got request Exceptions')
+    print('Sender:',sender)
+    print('environ:',request)
+    print(f'kwargs: {kwargs}')
+    
+@receiver(pre_migrate)
+def before_install_app(sender, app_config, verbosity, interactive, using, plan, apps, **kwargs):
+    print('================================')
+    print('Pre Migrate Signal')
+    print('Sender:',sender)
+    print('app_config:',app_config)
+    print('verbosity:',verbosity)
+    print('interactive:',interactive)
+    print('using:', using)
+    print('plan:',plan)
+    print('apps:',apps)
+    print(f'kwargs: {kwargs}')
+
+@receiver(post_migrate)
+def at_end_migrate_flush(sender, app_config, verbosity, interactive, using, plan, apps, **kwargs):
+    print('================================')
+    print('Post Migrate Signal')
+    print('Sender:',sender)
+    print('app_config:',app_config)
+    print('verbosity:',verbosity)
+    print('interactive:',interactive)
+    print('using:', using)
+    print('plan:',plan)
+    print('apps:',apps)
+    print(f'kwargs: {kwargs}')
